f00eaa025ef4f731e427b3159f3d2701e2862502
Danilo142 <d.cerna@alumno.um.edu.ar>
2022-11-14 19:15:51 -0300

    functions

---
+++ Frontend/main/__init__.py
@@ -0,0 +1,8 @@
+import os
+from flask import Flask
+from dotenv import load_dotenv
+from main.routes import main, poem, user
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
