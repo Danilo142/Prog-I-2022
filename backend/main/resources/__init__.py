18cb44b6001816214a1ee1ea795f1204d30b7715
Danilo142 <81604702+Danilo142@users.noreply.github.com>
2022-11-22 12:24:24 -0300

    Delete backend directory

--- backend/main/resources/__init__.py
+++
@@ -1,6 +0,0 @@
-from .poem import Poem as PoemResource
-from .poem import Poems as PoemsResource
-from .qualification import Qualifications as QualificationsResource
-from .qualification import Qualification as QualificationResource
-from .user import User as UserResource
-from .user import Users as UsersResource
